ffc1a070667ea2d671c92868e1ed7d27245a96b1
Long Le <lelong03@gmail.com>
2024-09-18 12:08:17 +0700

    Update new server response

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello Cloudy\n'; 
+const message = 'Hello Cloudy, How are you today!\n'; 
 const port = 8080; 
 const server = http.createServer((req, res) => { 
   res.statusCode = 200;
